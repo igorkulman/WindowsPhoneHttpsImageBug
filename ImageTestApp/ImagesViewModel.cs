fa1f3e06ad9470a018f025d9c756c7360d06d4ae
Igor Kulman <igor@kulman.sk>
2015-09-09 16:11:31 +0200

    * init commit

---
+++ ImageTestApp/ImagesViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageTestApp
+{
+    public class ImagesViewModel
+    {
+        public ObservableCollection<string> Images { get; }
+
+        private readonly string[] _images = {
+            "https://i.alza.cz/Foto/ImgGalery/Ikony/Mobilni_telefony/Wearables/zdravi.png",
+            "https://i.alza.cz/Foto/ImgGalery/IkonyPatra/Mobilni_telefony/120/wearables.png",
+            "https://i.alza.cz/Foto/category/120/18855742.png", "https://i.alza.cz/Foto/category/120/18855843.png",
+            "https://i.alza.cz/Foto/ImgGalery/IkonyPatra/Elektronika/120/sc.png",
+            "https://i.alza.cz/Foto/ImgGalery/IkonyPatra/Kategorie/Mobilni_telefony/Chytre_telefony.jpg",
+            "https://i.alza.cz/Foto/category/120/18856129.png",
+            "https://i.alza.cz/Foto/ImgGalery/IkonyPatra/Kategorie/Televizory/smart_tv.jpg",
+            "https://i.alza.cz/Foto/category/40/18855744_1.png"
+        };
+
+        public ImagesViewModel()
+        {
+            Images=new ObservableCollection<string>();            
+        }
+
+        public void LoadHttp()
+        {
+            Images.Clear();
+            foreach (var image in _images)
+            {
+                Images.Add(image.Replace("https:","http:"));
+            }
+        }
+
+        public void LoadHttps()
+        {           
+            Images.Clear();
+            foreach (var image in _images)
+            {
+                Images.Add(image);
+            }
+        }
+    }
+}
---
+++ ImageTestApp/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using ImageTestApp.Resources;
+
+namespace ImageTestApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private readonly ImagesViewModel _vm;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            _vm=new ImagesViewModel();
+            DataContext = _vm;
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.LoadHttps();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _vm.LoadHttp();
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
